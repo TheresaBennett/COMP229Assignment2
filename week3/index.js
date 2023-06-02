64d2d610e994a1dc46757334e26a581020a4eab2
Theresa Bennett <tbenne21@my.centennialcollege.ca>
2023-06-02 16:38:38 -0400

    week3 updates

--- week3/index.js
+++ week3/index.js
@@ -1,14 +1,19 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('goodbye cruel, World');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/info', (req, res) => {
+  res.send('site info');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('contact me');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
