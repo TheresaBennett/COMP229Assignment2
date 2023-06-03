dbcb8b9865e6e82df194df3c0ade25eea3f3f5b1
Theresa Bennett <tbenne21@my.centennialcollege.ca>
2023-06-02 22:53:39 -0400

    bootstrap and fontawesome updates

--- week3Part2/app.js
+++ week3Part2/app.js
@@ -12,14 +12,14 @@ let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // experss -e
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
