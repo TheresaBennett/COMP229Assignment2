99b86e1880a07789b995885a6b770af7899ef545
Theresa Bennett <tbenne21@my.centennialcollege.ca>
2023-06-06 13:10:28 -0400

    updates

--- week4/routes/index.js
+++ week4/routes/index.js
@@ -1,14 +1,16 @@
+// File name: Express app portfolio
+// Student’s Name: Theresa Bennett
+// StudentID: 300909345
+// Date: June 4th 2023
 var express = require('express');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home',});
 });
-// File name: Express app portfolio
-// Student’s Name: Theresa Bennett
-// StudentID: 300909345
-// Date: June 4th 2023
 
 
 /* GET home page. */
