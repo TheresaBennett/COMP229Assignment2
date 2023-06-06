1140b95fa6f010b087d5dea90a21bfb0f6e74426
Theresa Bennett <tbenne21@my.centennialcollege.ca>
2023-06-06 11:01:29 -0400

    week4 styling updates

---
+++ week4/public/scripts/app.js
@@ -0,0 +1,15 @@
+// File name: Express app portfolio
+// Student’s Name: Theresa Bennett
+// StudentID: 300909345
+// Date: June 4th 2023
+
+
+(function(){
+   
+    function Start ()
+    {
+        console.log("App Started...");
+    }
+    window.addEventListener("Load", Start);
+
+})();
